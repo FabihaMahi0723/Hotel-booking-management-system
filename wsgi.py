84a012316e6d28343cb1ab6c3c1627059e7c6f47
FabihaMahi0723 <52478562+FabihaMahi0723@users.noreply.github.com>
2019-07-10 00:57:25 -0700

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hbms project.
+WSGI config for mahi1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hbms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mahi1.settings')
 
 application = get_wsgi_application()
-
