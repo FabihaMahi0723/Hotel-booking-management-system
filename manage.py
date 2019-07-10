8ab9be32d9fc6ba1262816ef48f85330b0d56706
FabihaMahi0723 <52478562+FabihaMahi0723@users.noreply.github.com>
2019-07-10 00:56:51 -0700

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mahi.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mahi1.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
